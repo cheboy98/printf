c4a2a505163ba83005670091080910386b5b4af3
Cherno Jallow <maticjallow08@gmail.com>
2023-06-28 02:22:58 +0000

    Header file

---
+++ main.h
@@ -0,0 +1,37 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct format - structure to store the format of _printf
+ * @format: store the current format
+ * @f: function pointer to store the different format specifiers
+ *
+ */
+typedef struct _format
+{
+	char format;
+	(*f)(va_list);
+} _format;
+
+int _printf(const char *format, ...);
+
+int _putchar(char c);
+int _strlen(char *s);
+
+int _print_char(va_list);
+int _print_str(va_list);
+int _print_int(va_list);
+int _print_unsigned_int(va_list);
+int _print_bin(va_list);
+int _print_oct(va_list);
+int _print_hex_lower(va_list);
+int _print_hex_upper(va_list);
+int _print_pointer(va_list);
+int _print_rev_str(va_list);
+int _print_rot13(va_list);
+
+#endif

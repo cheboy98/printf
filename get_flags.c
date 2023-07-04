e2f1f65ce9892207e2b2cafdf13e9bc50dbe4d31
Mlu1432 <Luxturesekwati@gmail.com>
2023-07-04 12:20:55 -0700

    big print gives and the small print takes away

--- get_flags.c
+++ get_flags.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
@@ -10,6 +10,10 @@ int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
+	int F_MINUS = 1;
+	int F_PLUS = 2;
+	int F_ZERO = 4;
+	int F_HASH = 8;
 	int k, current_index;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
